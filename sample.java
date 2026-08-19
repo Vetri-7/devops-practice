400dc265ef9ffd672160f5ec22d9cd352a6c05a6
Vetriselvan-V <149142125+Vetriselvan-V@users.noreply.github.com>
2026-08-19 12:54:58 +0530

    intial commit

---
+++ sample.java
@@ -0,0 +1,7 @@
+public class sample{
+    public static void main(String[] args){
+        int var=0;
+        String str="hey there this is vetri";
+        System.out.print("my current skills is "+var);
+    }
+    }
